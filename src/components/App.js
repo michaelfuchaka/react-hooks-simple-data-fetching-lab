f7a93dd3ae24c0db1693d4844f1fdda5feb34b72
michaelfuchaka <fuchakamichael06@gmail.com>
2025-07-22 21:54:43 +0300

    Add App component to fetch and display a random dog image

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setImageUrl(data.message);
+      });
+  }, []);
+
+  if (!imageUrl) {
+    return <p>Loading...</p>;
+  }
+
+  return <img src={imageUrl} alt="A Random Dog" />;
+}
+
+export default App;
